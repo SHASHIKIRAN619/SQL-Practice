006318edc5b95180a1f4b93b67449a210e6bd40e
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-04-19 16:43:05 +0530

    lesson06_Multi_tables_join.sql

---
+++ sqlbolt/lesson06_Multi_tables_join.sql
@@ -0,0 +1,13 @@
+-- ---------------------------------------------------------------
+--Task 1: Find the domestic and international sales for each movie
+-- ---------------------------------------------------------------------------------------------------------------------
+ SELECT m.title, d.domestic_sales,d.international_salesfrom movies as m innner join boxoffice as b on m.id = b.movie_id;
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Task 2: Show the sales numbers for each movie that did better internationally rather than domestically
+-- ---------------------------------------------------------------------------------------------------------------------
+ SELECT m.title,b.international_sales,b.domestic_sales from movies as m inner join boxoffice as b on m.id = b.movie_id
+ where b.international_sales > b.domestic_sales;
+-- ---------------------------------------------------------------------------------------------------------------------
+-- Task 3: List all the movies by their ratings in descending orde
+-- ---------------------------------------------------------------------------------------------------------------------
+ SELECT m.title,b.rating from movies as m inner join boxoffice as b on m.id = b.movie_id order by b.rating desc;
