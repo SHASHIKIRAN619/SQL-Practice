d585e33b69587777c16917a9c2ae67cdf12c7d8e
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-04-23 17:45:49 +0530

    Create lesson16_Creating_Tables.sql

---
+++ sqlbolt/lesson16_Creating_Tables.sql
@@ -0,0 +1,15 @@
+-- =================================================
+-- SQLBolt Lesson 16: Creating Tales
+-- =================================================
+-- ------------------------------------------------------------------------------------------------
+Tasks: 
+Create a new table named Database with the following columns:
+– Name A string (text) describing the name of the database
+– Version A number (floating point) of the latest version of this database
+– Download_count An integer count of the number of times this database was downloaded
+-- -------------------------------------------------------------------------------------------------
+create table Database(
+Name varchar(20),
+Version float(2),
+Download_count integer
+);
