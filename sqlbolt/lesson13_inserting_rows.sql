687d61e71161e121efa475d887661536c65842c2
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-04-23 17:20:52 +0530

    Create lesson13_inserting_rows.sql

---
+++ sqlbolt/lesson13_inserting_rows.sql
@@ -0,0 +1,10 @@
+-- -----------------------------------------------------------------------------------------------------
+-- Task 1: Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+-- -----------------------------------------------------------------------------------------------------
+   Insert into movies values(11,'Toy Story 4','	John Lasseter',2011,105);
+-- ----------------------------------------------------------------------------------------------------
+-- Task 2: Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, 
+   and made 340 million domestically and 270 million internationally. 
+   Add the record to the BoxOffice table.
+-- ----------------------------------------------------------------------------------------------------
+   Insert into boxoffice values(11,8.7,340000000,270000000);
