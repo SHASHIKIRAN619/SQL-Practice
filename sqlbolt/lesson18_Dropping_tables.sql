6574ffa77d9eee68aedcd927142e9772f181d713
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-04-24 14:57:44 +0530

    Create lesson18_Dropping_tables.sql

---
+++ sqlbolt/lesson18_Dropping_tables.sql
@@ -0,0 +1,17 @@
+-- =================================================
+-- SQLBolt Lesson 18: Dropping tables 
+-- =================================================
+
+-- Table: movies
+-- Columns: id, title, director, year, length_minutes
+-- Table: Boxoffice
+-- Columns: movie_id, rating, Domestic_sales, international_sales
+
+-- -----------------------------------------------------------------------------------------------
+-- Task 1: We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+-- -----------------------------------------------------------------------------------------------
+   Drop table movies;
+-- -----------------------------------------------------------------------------------------------
+-- Task 2: And drop the BoxOffice table as well
+-- -----------------------------------------------------------------------------------------------
+   Drop table boxoffice;
