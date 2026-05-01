f41ec5309537ddaced2c568b6c13741e26db474c
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-05-01 18:21:30 +0530

    Update null_handling.sql

--- practice-set/null_handling.sql
+++ practice-set/null_handling.sql
@@ -15,3 +15,28 @@
 -- Task 4: Display name and salary, but if salary is NULL show 'No Salary'
 -- =========================================================================================
    SELECT name, salary, coalesce(salary,'No Salary') as salary from employees;
+-- =========================================================================================
+-- Task 5: Show employees where department is NULL
+-- =========================================================================================
+   SELECT * from employees where department is null;
+- ==========================================================================================
+-- Task 6: Show employees where city is NOT NULL
+-- ========================================================================================
+   SELECT * from employees where city is not null;
+-- ========================================================================================
+-- Task 7: Show employees where salary is NULL OR salary < 30000
+-- =======================================================================================
+   SELECT * from employees where salary is null or salary < 30000;
+-- =======================================================================================
+-- Task 8: Show employees where salary is NOT NULL AND salary > 50000
+-- ======================================================================================
+   SELECT * from employees where salary is not null and salary > 50000;
+-- ======================================================================================
+-- Task 9: Show employees where department is NULL AND salary is NULL
+-- ======================================================================================
+   SELECT * from employees where department is null and salary is null;
+-- =====================================================================================
+-- Task 10: Show employees where department is NOT NULL but city is NULL
+-- =====================================================================================
+   SELECT * from employees where department is not null and city is null;
+
