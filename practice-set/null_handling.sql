13d0b0e196c343c041877a88fbf6ad8137d22cb2
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-05-02 16:41:05 +0530

    Update null_handling.sql

--- practice-set/null_handling.sql
+++ practice-set/null_handling.sql
@@ -39,4 +39,29 @@
 -- Task 10: Show employees where department is NOT NULL but city is NULL
 -- =====================================================================================
    SELECT * from employees where department is not null and city is null;
+-- =====================================================================================
+-- Task 11: Replace NULL salary with 0
+-- =====================================================================================
+   SELECT name, COALESCE(salary,0) as salary from employees;
+-- =====================================================================================
+-- Task 12: Replace NULL salary with 10000
+-- ====================================================================================
+   SELECT name, COALESCE(salary, 10000) as salary from employees;
+-- ====================================================================================
+-- Task 13: Replace NULL city with 'Unknown'
+-- ====================================================================================
+   SELECT name, COALESCE(city, 'Unknown') as city from employees;
+-- ====================================================================================
+-- Task 14: Replace NULL department with 'Not Assigned'
+-- ====================================================================================
+   SELECT name, COALESCE(department, 'Not Assigned') as department from employees;
+-- ====================================================================================
+-- Task 15: Show name and salary, if salary NULL → 'No Salary'
+- =====================================================================================
+   SELECT name, COALESCE(CAST(salary as char), 'No salary')as salary from employees;
+-- ====================================================================================
+-- Task 16: Show name, city, department replacing all NULLs with proper labels
+-- ====================================================================================
+   SELECT name, COALESCE(city,'Unknown') as city, COALESCE(department,'Not Asigned') 
+   as department from employees;
 
