75b85dbbc76d2a352e7f220aef904ca4296ded23
shashi619 <98157823+SHASHIKIRAN619@users.noreply.github.com>
2026-05-06 15:57:01 +0530

    Create queries.sql

---
+++ practice-set/queries.sql
@@ -0,0 +1,43 @@
+-- ============================================
+-- Practice-Set Sql Queries.
+-- ============================================
+
+-- Table: Employees,departments,projects
+--Employees:- emp_id, name, age, salary, city, department_id;
+--departments:- department_id, department_name;
+--Projects:- project_id, project_name, emp_id;
+
+-- ====================================================================================
+-- Task 1: Display all columns from the employees table.
+-- ====================================================================================
+   SELECT * from employees;
+-- ====================================================================================
+-- Task 2: Display only name and salary of all employees.
+-- ====================================================================================
+   SELECT name, salary from employees;
+-- ====================================================================================
+-- Task 3: Display name, age, and city of employees.
+-- ====================================================================================
+   SELECT name, age,city from employees;
+-- ====================================================================================
+-- Task 4: Display all records from the departments table.
+-- ====================================================================================
+   SELECT * from departments;
+-- ====================================================================================
+-- Task 5: Display only project_name from the projects table.
+-- ====================================================================================
+   SELECT PROJECT_NAME FROM PROJECTS;
+-- =======================================================================================
+-- Task 6: Display name and salary, but rename them as employee_name and employee_salary.
+-- =======================================================================================
+   SELECT name as employee_name, salary as employee_salary from employees;
+-- =======================================================================================
+-- Task 7: Display all employees along with a new column showing salary + 10000.
+-- =======================================================================================
+   SELECT emp_id,name,age,salary,city,(salary + 10000) as salary_plus_amount from employees;
+-- =========================================================================================
+-- Task 8: Display name and age, but rename age as employee_age.
+-- ==========================================================================================
+   SELECT name, age as employee_age from employees;
+-- ===========================================================================================
+-- Task 9: 
